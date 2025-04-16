f3843bebda037a4b9bba1d5b698acaa381575fd1
Mathew <mathew_holland@hotmail.co.uk>
2025-04-16 20:33:38 +0100

    Added some font sizes and background colors

--- GUI.py
+++ GUI.py
@@ -1,8 +1,8 @@
 import FreeSimpleGUI as sG
-
-label = sG.Text("Enter a Todo:", text_color="white", background_color="black")
-input_box = sG.Input(tooltip="Enter todo", text_color="black")
-add_button = sG.Button("Add")
+font_size = 44
+label = sG.Text("Enter a Todo:", text_color="white", background_color="black", font=font_size)
+input_box = sG.Input(tooltip="Enter todo", text_color="black", font=font_size)
+add_button = sG.Button("Add", font=font_size)
 
 window = sG.Window("Todo-App", layout=[[label], [input_box, add_button]], background_color="black")
 window.read()
